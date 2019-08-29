56d8abe072fa71b1555adc34b74c4965a06a82fb
GMagalhaes <gmagalhaes.ribeiro@gmail.com>
2019-08-28 23:38:23 -0300

    Majon Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Drawer from '@material-ui/core/Drawer';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import AddIcon from '@material-ui/icons/Add';
+import {Switch, Route, Link} from 'react-router-dom'
+import DataList from './DataList'
+
+
+const drawerWidth = 20;
+
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}%)`,
+    marginLeft: drawerWidth,
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: `${drawerWidth}%`,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 0,
+    backgroundColor: theme.palette.background.default,
+    paddingLeft: `calc(${drawerWidth}% + 4%)`,
+    paddingTop: '2%',
+    width: '90%',
+  },
+}));
+
 
 function App() {
+  
+  const [open, setOpen] = React.useState(false);
+  
+  function handleDrawerOpen() {
+      setOpen(true);
+  }
+
+  function handleDrawerClose() {
+      setOpen(false);
+  }
+  const UpBar = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    return(
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Lab
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+  
+  const DrawBar = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    return (
+      <Drawer
+      className={classes.drawer}
+      variant="permanent"
+      classes={{
+        paper: classes.drawerPaper,
+      }}
+      anchor="left"
+    >
+      <div className={classes.toolbar}>
+        
+      </div>
+      <Divider />
+      <List>
+        {['New Record', 'List'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <AddIcon /> : <ListAltIcon />}</ListItemIcon>
+            <ListItemText> 
+                <Link to="/list">
+                    {text}
+                </Link>
+            </ListItemText> 
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
+    )}
+  
+  const Main = () =>{
+    const classes = useStyles();
+    const theme = useTheme();
+    
+    return(
+      <main className={classes.content}>
+      <div className={classes.toolbar} />
+      
+      <Switch>
+        {/*<Route exact path='/' component={Home}/>*/}
+        <Route path='/list' component={DataList}/>
+        {/*<Route path='/add' component={Insert}/>*/}
+      </Switch>
+    </main>
+     )
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <React.Fragment>
+          <CssBaseline />
+          <UpBar />
+          <DrawBar />
+          <Main />
+        </React.Fragment> 
     </div>
   );
 }
---
+++ src/DataList.js
@@ -0,0 +1,124 @@
+import React, {Component} from 'react'
+import { makeStyles, useTheme} from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button'
+import { Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      marginTop: theme.spacing(3),
+      overflowX: 'auto',
+      padding: theme.spacing(3, 3)
+    },
+    table: {
+      minWidth: 650,
+      width: '80%',
+    },
+  }));
+  
+const TableHeader = () => {
+    const classes = useStyles();
+    const theme = useTheme();
+
+    return (
+        <TableHead>
+        <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell align="right"></TableCell>
+            
+        </TableRow>
+        </TableHead>
+    )
+}
+
+const TableRows = props => {
+
+    const classes = useStyles();
+    const theme = useTheme();
+    const rows = props.data.map((row, index) => {
+        return(
+            <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                    {row.name}
+                </TableCell>
+                <TableCell align="right">{row.calories}</TableCell>
+                <TableCell align="right">{row.fat}</TableCell>
+                <TableCell align="right">{row.carbs}</TableCell>
+                <TableCell align="right">{row.protein}</TableCell>
+                <TableCell align="right">
+                <Button variant="contained"size="medium">
+                    Edit
+                </Button>
+                </TableCell>
+            </TableRow>
+        );
+    });
+
+    return (
+        <TableBody>
+            {rows}
+        </TableBody>
+    )
+}
+
+const GenTable = props => {
+
+    const theme = useTheme();
+    const classes = useStyles();
+    const {data} = props
+    
+    return (
+        <Paper className={classes.root}>
+            <Typography variant="h4" gutterBottom>
+                Element List
+            </Typography>
+            <Table>
+                <TableHeader />
+                <TableRows data={data} />
+            </Table> 
+        </Paper>    
+    )   
+}
+
+class DataList extends Component {
+    constructor(props) {
+        super(props);
+        
+
+        let data = [
+            this.createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+            this.createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+            this.createData('Eclair', 262, 16.0, 24, 6.0),
+            this.createData('Cupcake', 305, 3.7, 67, 4.3),
+            this.createData('Gingerbread', 356, 16.0, 49, 3.9),
+        ];
+        this.state = {
+            data: data,
+        };
+    };
+    
+    createData(name, calories, fat, carbs, protein) {
+        return { name, calories, fat, carbs, protein };
+    };
+
+
+    render(){
+        const {data} = this.state
+        return(
+            <GenTable data={data}/>
+        )
+    }
+}
+
+
+export default DataList
